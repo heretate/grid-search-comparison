fd0d03319dcc659eaf3b6b0a4863c68314aa4047
Eric Huan <yxhuan@gatech.edu>
2022-06-05 22:13:18 -0400

    Added: A lot :)

---
+++ src/components/mainContent/subtopbar/Subtopbar.jsx
@@ -0,0 +1,11 @@
+import "./subtopbar.css"
+
+export default function Subtopbar() {
+  return (
+    <div className="subtopbar">
+        <div className="topbarWrapper">
+           
+        </div>
+    </div>
+  )
+}
